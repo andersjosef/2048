0fbccf869c15b4918a4368a0f1fa80253ef9636b
Anders <a-tunglan@hotmail.com>
2024-03-13 17:47:44 +0100

    board moves, but does not scale

--- twenty48/fullscreen.go
+++ twenty48/fullscreen.go
@@ -24,4 +24,18 @@ func (b *Board) ToggleFullScreen() {
 		ebiten.SetFullscreen(true)
 		b.game.screenControl.fullscreen = true
 	}
+	b.game.screenSizeChanged = true
+}
+
+func (g *Game) ChangeBoardPosition() {
+	var newWidth, newHeight int
+	if g.screenControl.fullscreen { // changing to full screen
+		newWidth, newHeight = ebiten.ScreenSizeInFullscreen()
+	} else { // changing to small
+		newWidth, newHeight = SCREENWIDTH, SCREENHEIGHT
+	}
+	start_pos_x = float32((newWidth - (BOARDSIZE * int(TILESIZE))) / 2)
+	start_pos_y = float32((newHeight - (BOARDSIZE * int(TILESIZE))) / 2)
+	g.board.createBoardImage()
+	g.screenSizeChanged = false
 }
