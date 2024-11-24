71f79c207a8a1afa57cc0d62451f029c2b10e4d3
Anders <a-tunglan@hotmail.com>
2024-11-24 18:17:08 +0100

    modular themes

--- twenty48/board.go
+++ twenty48/board.go
@@ -5,6 +5,7 @@ import (
 	"image/color"
 	"math/rand"
 
+	"github.com/andersjosef/2048/twenty48/theme"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/hajimehoshi/ebiten/v2/vector"
@@ -33,14 +34,9 @@ func NewBoard(g *Game) (*Board, error) {
 	b := &Board{}
 
 	// border and background colors
-	if g.darkMode { // INIT in DARK MODE
-		b.colorBorder = colorBorderDarkMode
-		b.colorBackgroundTile = colorBackgroundTileDarkMode
+	b.colorBorder = g.currentTheme.ColorBorder
+	b.colorBackgroundTile = g.currentTheme.ColorBackgroundTile
 
-	} else { // INIT in DEFAULT MODE
-		b.colorBorder = colorBorderDefault
-		b.colorBackgroundTile = colorBackgroundTileDefault
-	}
 	b.game = g
 	// add the two start pieces
 	for i := 0; i < 2; i++ {
@@ -93,12 +89,8 @@ func (b *Board) DrawTile(screen *ebiten.Image, startX, startY float32, x, y int,
 
 	if value != 0 {
 		// Set tile color to default color
-		colorMap := colorMapDefault
+		colorMap := b.game.currentTheme.ColorMap
 
-		// change tile colors to dark if darkmode is activated
-		if b.game.darkMode {
-			colorMap = colorMapDarkMode
-		}
 		val, ok := colorMap[value] // checks if num in map, if it is make the background else draw normal
 
 		if ok { // If the key exists draw the coresponding color background
@@ -128,7 +120,7 @@ func (b *Board) DrawNumberBackground(screen *ebiten.Image, startX, startY float3
 		size_tile float32 = (float32(TILESIZE) - BORDERSIZE) * float32(b.game.scale)
 	)
 	vector.DrawFilledRect(screen, xpos, ypos,
-		size_tile, size_tile, getColor(val), false) // tiles
+		size_tile, size_tile, theme.GetColor(val), false) // tiles
 }
 
 func (b *Board) DrawText(screen *ebiten.Image, xpos, ypos float32, x, y int, value int) {
@@ -156,7 +148,7 @@ func (b *Board) DrawText(screen *ebiten.Image, xpos, ypos float32, x, y int, val
 	text.Draw(screen, msg, fontUsed,
 		textPosX,
 		textPosY,
-		colorText)
+		b.game.currentTheme.ColorText)
 }
 
 // the functions for adding a random piece if the board is
---
+++ twenty48/theme/theme.go
@@ -0,0 +1,117 @@
+package theme
+
+import (
+	"image/color"
+)
+
+type Theme struct {
+	ColorText             color.RGBA
+	ColorBorder           color.RGBA
+	ColorBackgroundTile   color.RGBA
+	ColorScreenBackground color.RGBA
+	ColorMap              map[int][4]uint8
+}
+
+var DefaultTheme = Theme{
+	ColorText:             color.RGBA{110, 93, 71, 255},
+	ColorBorder:           color.RGBA{194, 182, 169, 255},
+	ColorBackgroundTile:   color.RGBA{204, 192, 179, 255},
+	ColorScreenBackground: color.RGBA{212, 200, 182, 255},
+	ColorMap: map[int][4]uint8{
+		2:     {238, 228, 218, 255},
+		4:     {237, 224, 200, 255},
+		8:     {242, 177, 121, 255},
+		16:    {245, 149, 99, 255},
+		32:    {255, 104, 69, 255},
+		64:    {246, 94, 59, 255},
+		128:   {237, 207, 114, 255},
+		256:   {237, 205, 100, 255},
+		512:   {237, 204, 97, 255},
+		1024:  {237, 200, 80, 255},
+		2048:  {237, 197, 63, 255},
+		4096:  {149, 189, 126, 255},
+		8192:  {107, 127, 95, 255},
+		16384: {247, 104, 104, 255},
+		-1:    {255, 255, 255, 255},
+	},
+}
+
+var DarkTheme = Theme{
+	ColorText:             color.RGBA{110, 93, 71, 255},
+	ColorBorder:           color.RGBA{154, 142, 129, 255},
+	ColorBackgroundTile:   color.RGBA{164, 152, 139, 255},
+	ColorScreenBackground: color.RGBA{0, 100, 102, 255},
+	ColorMap: map[int][4]uint8{
+		2:     {218, 208, 198, 255},
+		4:     {217, 204, 180, 255},
+		8:     {222, 157, 101, 255},
+		16:    {225, 129, 79, 255},
+		32:    {235, 84, 49, 255},
+		64:    {226, 74, 39, 255},
+		128:   {217, 187, 94, 255},
+		256:   {217, 185, 80, 255},
+		512:   {217, 184, 77, 255},
+		1024:  {217, 180, 60, 255},
+		2048:  {217, 177, 43, 255},
+		4096:  {129, 169, 106, 255},
+		8192:  {87, 107, 75, 255},
+		16384: {227, 84, 84, 255},
+		-1:    {255, 255, 255, 255},
+	},
+}
+
+// var (
+// colorText = color.RGBA{110, 93, 71, 255}
+// // DEFAULT
+// colorBorderDefault           = color.RGBA{194, 182, 169, 255}
+// colorBackgroundTileDefault   = color.RGBA{204, 192, 179, 255}
+// screenBackgroundColorDefault = [4]uint8{212, 200, 182, 255}
+
+// DARK MODE
+// screenBackgroundColorDarkMode = [4]uint8{0, 100, 102, 255}
+// colorBorderDarkMode           = color.RGBA{154, 142, 129, 255}
+// colorBackgroundTileDarkMode   = color.RGBA{164, 152, 139, 255}
+// )
+
+// colors for different numbers DEFAULT/LIGHT MODE
+// var colorMapDefault = map[int][4]uint8{
+// 	2:     {238, 228, 218, 255},
+// 	4:     {237, 224, 200, 255},
+// 	8:     {242, 177, 121, 255},
+// 	16:    {245, 149, 99, 255},
+// 	32:    {255, 104, 69, 255},
+// 	64:    {246, 94, 59, 255},
+// 	128:   {237, 207, 114, 255},
+// 	256:   {237, 205, 100, 255},
+// 	512:   {237, 204, 97, 255},
+// 	1024:  {237, 200, 80, 255},
+// 	2048:  {237, 197, 63, 255},
+// 	4096:  {149, 189, 126, 255},
+// 	8192:  {107, 127, 95, 255},
+// 	16384: {247, 104, 104, 255},
+// 	-1:    {255, 255, 255, 255},
+// }
+
+// colors for different numbers DARK MODE
+// var colorMapDarkMode = map[int][4]uint8{
+// 	2:     {218, 208, 198, 255},
+// 	4:     {217, 204, 180, 255},
+// 	8:     {222, 157, 101, 255},
+// 	16:    {225, 129, 79, 255},
+// 	32:    {235, 84, 49, 255},
+// 	64:    {226, 74, 39, 255},
+// 	128:   {217, 187, 94, 255},
+// 	256:   {217, 185, 80, 255},
+// 	512:   {217, 184, 77, 255},
+// 	1024:  {217, 180, 60, 255},
+// 	2048:  {217, 177, 43, 255},
+// 	4096:  {129, 169, 106, 255},
+// 	8192:  {87, 107, 75, 255},
+// 	16384: {227, 84, 84, 255},
+// 	-1:    {255, 255, 255, 255},
+// }
+
+func GetColor(colorList [4]uint8) color.RGBA {
+	c := color.RGBA{colorList[0], colorList[1], colorList[2], colorList[3]}
+	return c
+}
