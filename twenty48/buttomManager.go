918f72f378425ab12a5af62c27721eeba3b5b8b7
Anders <a-tunglan@hotmail.com>
2024-11-24 16:21:11 +0100

    button is its own file now

--- twenty48/buttomManager.go
+++ twenty48/buttomManager.go
@@ -1,12 +1,9 @@
 package twenty48
 
 import (
-	"fmt"
 	"image/color"
-	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 	"golang.org/x/image/font"
 )
@@ -34,15 +31,6 @@ func InitButtonManager(g *Game) *ButtonManager {
 
 // Initiaze buttons here with Addbutton
 func (bm *ButtonManager) initButtons() {
-
-	// Testbutton
-	// bm.AddButton(
-	// 	"Test Button!",
-	// 	[2]int{200, 200},
-	// 	mplusNormalFontMini,
-	// 	testForButtonAction,
-	// 	StateMainMenu,
-	// )
 
 	// Main Menu
 	bm.AddButton(
@@ -149,64 +137,4 @@ func (bm *ButtonManager) AddButton(buttonText string, startPos [2]int, font font
 
 	// Store text as key for access other places in the code
 	bm.buttonKeyMap[newButton.text] = newButton
-}
-
-// // Button ////
-type Button struct {
-	game           *Game
-	startPos       [2]int
-	endPos         [2]int
-	text           string
-	font           font.Face
-	actionFunction ActionFunc
-}
-
-func (bu *Button) UpdatePos(posX, posY int) {
-	dx, dy, err := bu.getDimentions()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	scale := bu.game.scale
-
-	var textLengt int = (dx / 2) / int(scale)
-	var textWidth int = (dy / 2) / int(scale)
-
-	bu.startPos = [2]int{
-		posX - textLengt,
-		posY - textWidth,
-	}
-	bu.endPos = [2]int{
-		posX + textLengt,
-		posY + textWidth,
-	}
-
-	fmt.Printf("Button bounds - startPos: %v, endPos: %v\n", bu.startPos, bu.endPos)
-}
-
-func (bu *Button) cursorWithin(curX, curY int) bool {
-	scale := ebiten.Monitor().DeviceScaleFactor()
-	curX = int(float64(curX) / scale)
-	curY = int(float64(curY) / scale)
-
-	if curX >= bu.startPos[0] && curX <= bu.endPos[0] {
-		if curY >= bu.startPos[1] && curY <= bu.endPos[1] {
-			return true
-		}
-	}
-	return false
-}
-
-func (b *Button) getDimentions() (int, int, error) {
-	if b.font == nil {
-		return -1, -1, fmt.Errorf("cant get dimentions, font is not set")
-	}
-	var x int = text.BoundString(b.font, b.text).Dx()
-	var y int = text.BoundString(b.font, b.text).Dy()
-
-	return x, y, nil
-}
-
-func (bu *Button) onTrigger() {
-	bu.actionFunction(bu.game.input)
 }
