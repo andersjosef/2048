d04ae6d58fd009b30a7b8374d627349d783cd554
andersjosef <a-tunglan@hotmail.com>
2025-06-15 12:54:00 +0200

    moves updated somewhat, no animations

--- twenty48/animations/animations.go
+++ twenty48/animations/animations.go
@@ -8,9 +8,18 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// Describes a single tile moving or merging from one tile to another
+type MoveDelta struct {
+	FromRow, FromCol int
+	ToRow, ToCol     int
+	ValueMoved       int
+	Merged           bool
+}
+
 type Animation struct {
 	isAnimating     bool
 	ArrayOfChange   [co.BOARDSIZE][co.BOARDSIZE]int // TMP exposure!
+	deltas          []MoveDelta
 	view            View
 	currentDir      string
 	animationLength float32           //seconds
@@ -34,34 +43,60 @@ func InitAnimation(g View) *Animation {
 	return a
 }
 
+func (a *Animation) Play(deltas []MoveDelta, dir string) {
+	// a.deltas = deltas
+	// a.currentDir = dir
+	// a.isAnimating = true
+	// a.startTime = time.Now()
+}
+
 func (a *Animation) Draw(screen *ebiten.Image) {
 	// Draw the backgroundimage of the game
 	a.view.DrawBackgoundBoard(screen)
 
 	// Calculate animation progress based on time since start
-	timeSinceStart := time.Since(a.startTime)
-	progress := min(float32(timeSinceStart.Seconds())/a.animationLength, 1)
+	elapsed := float32(time.Since(a.startTime).Seconds())
+	progress := float32(min(float64(elapsed/a.animationLength), 1))
 
-	// Draw tiles for animation
-	mWidth, mHeight := a.view.GetBoardDimentions()
-	for y := range mHeight {
-		for x := range mWidth {
-			var (
-				movingDistX float32 = progress * float32(a.directionMap[a.currentDir][0]) * float32(co.BOARDSIZE-1)
-				movingDistY float32 = progress * float32(a.directionMap[a.currentDir][1]) * float32(co.BOARDSIZE-1)
-			)
-			if math.Abs(float64(movingDistX)) >= float64(a.ArrayOfChange[y][x]) || math.Abs(float64(movingDistY)) >= float64(a.ArrayOfChange[y][x]) {
-				movingDistX = float32(a.directionMap[a.currentDir][0]) * float32(a.ArrayOfChange[y][x])
-				movingDistY = float32(a.directionMap[a.currentDir][1]) * float32(a.ArrayOfChange[y][x])
-			}
-			a.view.DrawMovingMatrix(
-				screen,
-				x,
-				y,
-				movingDistX,
-				movingDistY,
-			)
-		}
+	// // Draw tiles for animation
+	// mWidth, mHeight := a.view.GetBoardDimentions()
+	// for y := range mHeight {
+	// 	for x := range mWidth {
+	// 		var (
+	// 			movingDistX float32 = progress * float32(a.directionMap[a.currentDir][0]) * float32(co.BOARDSIZE-1)
+	// 			movingDistY float32 = progress * float32(a.directionMap[a.currentDir][1]) * float32(co.BOARDSIZE-1)
+	// 		)
+	// 		if math.Abs(float64(movingDistX)) >= float64(a.ArrayOfChange[y][x]) || math.Abs(float64(movingDistY)) >= float64(a.ArrayOfChange[y][x]) {
+	// 			movingDistX = float32(a.directionMap[a.currentDir][0]) * float32(a.ArrayOfChange[y][x])
+	// 			movingDistY = float32(a.directionMap[a.currentDir][1]) * float32(a.ArrayOfChange[y][x])
+	// 		}
+	// 		a.view.DrawMovingMatrix(
+	// 			screen,
+	// 			x,
+	// 			y,
+	// 			movingDistX,
+	// 			movingDistY,
+	// 		)
+	// 	}
+	// }
+
+	for _, d := range a.deltas {
+		// How many cells we move in X/Y
+		dx := float32(d.ToCol - d.FromCol)
+		dy := float32(d.ToRow - d.FromRow)
+
+		// Progress * total cells, clamped so no overshoot
+		movingDistX := float32(math.Copysign(math.Min(math.Abs(float64(dx*progress)), math.Abs(float64(dx))), float64(dx)))
+		movingDistY := float32(math.Copysign(math.Min(math.Abs(float64(dy*progress)), math.Abs(float64(dy))), float64(dy)))
+
+		// Draw at the original grid cell + offset
+		a.view.DrawMovingMatrix(
+			screen,
+			d.FromCol,
+			d.FromRow,
+			movingDistX,
+			movingDistY,
+		)
 	}
 
 	if progress >= 1 {
--- twenty48/moves.go
+++ twenty48/moves.go
@@ -1,89 +1,229 @@
 package twenty48
 
-import co "github.com/andersjosef/2048/twenty48/constants"
+import (
+	"github.com/andersjosef/2048/twenty48/animations"
+	co "github.com/andersjosef/2048/twenty48/constants"
+)
 
 func (b *Board) updateBoardBeforeChange() {
 	b.matrixBeforeChange = b.matrix
 }
 
+// func (b *Board) moveLeft() {
+// 	b.updateBoardBeforeChange()
+// 	for i := range b.matrix {
+// 		// Shift tiles to the left
+// 		compactTiles(i, b, true)
+// 		// Merge tiles and shift again if needed
+// 		mergeTiles(&b.matrix[i], b)
+// 		compactTiles(i, b, false)
+// 	}
+// 	b.game.animation.ActivateAnimation("LEFT")
+
+// 	b.addNewRandomPieceIfBoardChanged()
+
+// 	b.game.gameOver = b.isGameOver()
+// }
+
 func (b *Board) moveLeft() {
 	b.updateBoardBeforeChange()
-	for i := range b.matrix {
-		// Shift tiles to the left
-		compactTiles(i, b, true)
-		// Merge tiles and shift again if needed
-		mergeTiles(&b.matrix[i], b)
-		compactTiles(i, b, false)
+
+	var allDeltas []animations.MoveDelta
+	var newMatrix [4][4]int
+
+	for rowIndex, row := range b.matrix {
+		slideRow, d1 := compactRow(rowIndex, row)
+
+		mergedRow, d2, scoreGain := mergeRow(rowIndex, slideRow)
+		b.game.score += scoreGain
+
+		finalRow, d3 := compactRow(rowIndex, mergedRow)
+
+		// Write to new matrix
+		newMatrix[rowIndex] = finalRow
+
+		// Collect all deltas
+		allDeltas = append(allDeltas, d1...)
+		allDeltas = append(allDeltas, d2...)
+		allDeltas = append(allDeltas, d3...)
+
 	}
-	b.game.animation.ActivateAnimation("LEFT")
+	b.game.animation.Play(allDeltas, "LEFT")
 
+	b.matrix = newMatrix
 	b.addNewRandomPieceIfBoardChanged()
-
 	b.game.gameOver = b.isGameOver()
 }
 
+// func (b *Board) moveUp() {
+// 	b.updateBoardBeforeChange()
+// 	transpose(&b.matrix)
+// 	for i := range b.matrix {
+// 		// Shift tiles "left" (actually up, due to transposition)
+// 		compactTiles(i, b, true)
+// 		// Merge tiles and shift again if needed
+// 		mergeTiles(&b.matrix[i], b)
+// 		compactTiles(i, b, false)
+// 	}
+// 	transpose(&b.matrix) // Transpose back to the original orientation
+// 	transpose(&b.game.animation.ArrayOfChange)
+// 	b.game.animation.ActivateAnimation("UP")
+
+// 	b.addNewRandomPieceIfBoardChanged()
+
+// 	b.game.gameOver = b.isGameOver()
+// }
+
 func (b *Board) moveUp() {
 	b.updateBoardBeforeChange()
-	transpose(&b.matrix)
-	for i := range b.matrix {
-		// Shift tiles "left" (actually up, due to transposition)
-		compactTiles(i, b, true)
-		// Merge tiles and shift again if needed
-		mergeTiles(&b.matrix[i], b)
-		compactTiles(i, b, false)
+	var snapShot [4][4]int
+	copy(snapShot[:], b.matrix[:])
+
+	var allDeltas []animations.MoveDelta
+	var newMatrix [4][4]int
+
+	transpose(&snapShot)
+	for rowIndex, row := range snapShot {
+		slideRow, d1 := compactRow(rowIndex, row)
+
+		mergedRow, d2, scoreGain := mergeRow(rowIndex, slideRow)
+		b.game.score += scoreGain
+
+		finalRow, d3 := compactRow(rowIndex, mergedRow)
+
+		// Write to new matrix
+		newMatrix[rowIndex] = finalRow
+
+		// Collect all deltas
+		allDeltas = append(allDeltas, d1...)
+		allDeltas = append(allDeltas, d2...)
+		allDeltas = append(allDeltas, d3...)
+
 	}
-	transpose(&b.matrix) // Transpose back to the original orientation
-	transpose(&b.game.animation.ArrayOfChange)
-	b.game.animation.ActivateAnimation("UP")
+	transpose(&newMatrix)
+	b.game.animation.Play(allDeltas, "UP")
 
+	b.matrix = newMatrix
 	b.addNewRandomPieceIfBoardChanged()
-
 	b.game.gameOver = b.isGameOver()
 }
 
 // TODO separate out logic on all these...
+// func (b *Board) moveRight() {
+// 	b.updateBoardBeforeChange()
+// 	for i := range b.matrix {
+// 		// Reverse the row to treat the right end as the left
+// 		reverseRow(&b.matrix[i])
+// 		// Shift tiles "left" (actually right, due to reversal)
+// 		compactTiles(i, b, true)
+// 		// Merge tiles and shift again if needed
+// 		mergeTiles(&b.matrix[i], b)
+// 		compactTiles(i, b, false)
+// 		// Reverse back to original orientation
+// 		reverseRow(&b.matrix[i])
+// 		reverseRow(&b.game.animation.ArrayOfChange[i])
+
+// 		b.game.animation.ActivateAnimation("RIGHT")
+// 	}
+
+// 	b.addNewRandomPieceIfBoardChanged()
+
+// 	b.game.gameOver = b.isGameOver()
+// }
+
 func (b *Board) moveRight() {
 	b.updateBoardBeforeChange()
-	for i := range b.matrix {
-		// Reverse the row to treat the right end as the left
-		reverseRow(&b.matrix[i])
-		// Shift tiles "left" (actually right, due to reversal)
-		compactTiles(i, b, true)
-		// Merge tiles and shift again if needed
-		mergeTiles(&b.matrix[i], b)
-		compactTiles(i, b, false)
-		// Reverse back to original orientation
-		reverseRow(&b.matrix[i])
-		reverseRow(&b.game.animation.ArrayOfChange[i])
+	var snapShot [4][4]int
+	copy(snapShot[:], b.matrix[:])
 
-		b.game.animation.ActivateAnimation("RIGHT")
+	var allDeltas []animations.MoveDelta
+	var newMatrix [4][4]int
+
+	for rowIndex, row := range snapShot {
+		reverseRow(&row)
+		slideRow, d1 := compactRow(rowIndex, row)
+
+		mergedRow, d2, scoreGain := mergeRow(rowIndex, slideRow)
+		b.game.score += scoreGain
+
+		finalRow, d3 := compactRow(rowIndex, mergedRow)
+
+		reverseRow(&finalRow)
+
+		// Write to new matrix
+		newMatrix[rowIndex] = finalRow
+
+		// Collect all deltas
+		allDeltas = append(allDeltas, d1...)
+		allDeltas = append(allDeltas, d2...)
+		allDeltas = append(allDeltas, d3...)
+
 	}
+	b.game.animation.Play(allDeltas, "RIGHT")
 
+	b.matrix = newMatrix
 	b.addNewRandomPieceIfBoardChanged()
-
 	b.game.gameOver = b.isGameOver()
 }
+
+// func (b *Board) moveDown() {
+// 	b.updateBoardBeforeChange()
+// 	transpose(&b.matrix)
+// 	for i := range b.matrix {
+// 		// Reverse the row (which is actually a column due to transposition)
+// 		reverseRow(&b.matrix[i])
+// 		// Shift tiles "left" (actually down, due to reversal and transposition)
+// 		compactTiles(i, b, true)
+// 		// Merge tiles and shift again if needed
+// 		mergeTiles(&b.matrix[i], b)
+// 		compactTiles(i, b, false)
+// 		// Reverse back to treat the bottom as the top
+// 		reverseRow(&b.matrix[i])
+// 		reverseRow(&b.game.animation.ArrayOfChange[i])
+// 	}
+// 	transpose(&b.matrix) // Transpose back to the original orientation
+// 	transpose(&b.game.animation.ArrayOfChange)
+// 	b.game.animation.ActivateAnimation("DOWN")
+
+// 	b.addNewRandomPieceIfBoardChanged()
+
+// 	b.game.gameOver = b.isGameOver()
+// }
+
 func (b *Board) moveDown() {
 	b.updateBoardBeforeChange()
-	transpose(&b.matrix)
-	for i := range b.matrix {
-		// Reverse the row (which is actually a column due to transposition)
-		reverseRow(&b.matrix[i])
-		// Shift tiles "left" (actually down, due to reversal and transposition)
-		compactTiles(i, b, true)
-		// Merge tiles and shift again if needed
-		mergeTiles(&b.matrix[i], b)
-		compactTiles(i, b, false)
-		// Reverse back to treat the bottom as the top
-		reverseRow(&b.matrix[i])
-		reverseRow(&b.game.animation.ArrayOfChange[i])
+	var snapShot [4][4]int
+	copy(snapShot[:], b.matrix[:])
+
+	var allDeltas []animations.MoveDelta
+	var newMatrix [4][4]int
+
+	transpose(&snapShot)
+	for rowIndex, row := range snapShot {
+		reverseRow(&row)
+		slideRow, d1 := compactRow(rowIndex, row)
+
+		mergedRow, d2, scoreGain := mergeRow(rowIndex, slideRow)
+		b.game.score += scoreGain
+
+		finalRow, d3 := compactRow(rowIndex, mergedRow)
+
+		reverseRow(&finalRow)
+
+		// Write to new matrix
+		newMatrix[rowIndex] = finalRow
+
+		// Collect all deltas
+		allDeltas = append(allDeltas, d1...)
+		allDeltas = append(allDeltas, d2...)
+		allDeltas = append(allDeltas, d3...)
+
 	}
-	transpose(&b.matrix) // Transpose back to the original orientation
-	transpose(&b.game.animation.ArrayOfChange)
-	b.game.animation.ActivateAnimation("DOWN")
+	transpose(&newMatrix)
+	b.matrix = newMatrix
+	b.game.animation.Play(allDeltas, "DOWN")
 
 	b.addNewRandomPieceIfBoardChanged()
-
 	b.game.gameOver = b.isGameOver()
 }
 
@@ -138,4 +278,55 @@ func transpose(board *[co.BOARDSIZE][co.BOARDSIZE]int) {
 			(*board)[i][j], (*board)[j][i] = (*board)[j][i], (*board)[i][j]
 		}
 	}
+}
+
+func compactRow(rowIndex int, row [4]int) (newRow [4]int, deltas []animations.MoveDelta) {
+	insertPos, lastVal, extraMov := 0, -1, 0
+
+	for col, val := range row {
+		if val == 0 {
+			continue
+		}
+		if val == lastVal {
+			extraMov++
+		}
+
+		// Record how far it will slide
+		delta := animations.MoveDelta{
+			FromRow: rowIndex, FromCol: col,
+			ToRow: rowIndex, ToCol: col,
+			ValueMoved: val,
+		}
+		if extraMov > 0 {
+			delta.ToCol -= extraMov
+		}
+		// Append the delta to deltas for animation
+		deltas = append(deltas, delta)
+
+		newRow[insertPos] = val
+		insertPos++
+		lastVal = val
+	}
+
+	return newRow, deltas
+}
+
+func mergeRow(rowIndex int, row [4]int) (newRow [4]int, deltas []animations.MoveDelta, scoreGain int) {
+	copy(newRow[:], row[:]) // Copy row info over in new row
+
+	for i := range 3 {
+		if newRow[i] != 0 && newRow[i] == newRow[i+1] {
+			newRow[i] *= 2         // Update number
+			scoreGain += newRow[i] // Update score
+			deltas = append(deltas, animations.MoveDelta{
+				FromRow: rowIndex, FromCol: i + 1,
+				ToRow: rowIndex, ToCol: i,
+				ValueMoved: newRow[i],
+				Merged:     true,
+			})
+			newRow[i+1] = 0 // Remove value that was merged into current val
+			i++             // Skip to next possible val spot
+		}
+	}
+	return newRow, deltas, scoreGain
 }
